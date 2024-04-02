12b73d93dd8652a59cf54b43cffe0758502f1b83
amfairley <adamfairley1990@gmail.com>
2024-04-02 08:43:41 +0100

    function: add game object

---
+++ scripts/game.js
@@ -0,0 +1,23 @@
+// game object, need it to contain the score, current game, player moves, and choices
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"],
+}
+// newGame()
+
+// addTurn()
+
+// showTurns()
+
+// lightsOn()
+
+// playerTurn()
+
+// showScore()
+
+// Export functions to test
+module.exports = {
+    game
+};
